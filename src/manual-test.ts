65ca1c4942473bed4c362d75bc9e5fbead4b1bca
Hamid Nazari <nazarihamid490@gmail.com>
2026-07-08 17:21:03 +0200

    feat: update manual test query to filter active users and limit results

--- src/manual-test.ts
+++ src/manual-test.ts
@@ -23,16 +23,21 @@ store.insert("users", [integer(4), text("Diana"), integer(28), boolean(true)]);
 
 
 
-const lexer = new Lexer("SELECT * FROM users");
+const lexer = new Lexer( `SELECT name, age FROM users WHERE active = TRUE ORDER BY age DESC LIMIT 2;`);
 
 const tokens = lexer.tokenize()
+
+console.log("Tokenizer \n")
 console.log(tokens)
 
 const parser = new Parser(tokens);
 const statement = parser.parse();
 
+console.log("\nParser \n")
 console.log(statement)
 
 const result = executeSelect(statement as SelectStatement, store);
 
-console.log(result);
+console.log("\nExecutor \n")
+// console.log(JSON.stringify(result, null, 2));
+console.log(result.columns, result.rows)
